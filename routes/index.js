32d40bfd32e1012246d33ff9f176e38a5ceef60f
Kavita <bkavita1998@gmail.com>
2024-11-13 15:32:47 +0100

    changement clé api

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const API_KEY= "e23234995348e7a610fe255fcbe001ef";
 
 
 router.get('/movies', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}`)
    .then(response => response.json())
    .then(data => {
      res.json({movies : data.results});
